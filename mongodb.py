493407aa960ec78217014595647ddab887b8ba5a
hecdelatorre <hector982015@gmail.com>
2022-04-15 15:06:56 -0500

    Add lamda functions

--- mongodb.py
+++ mongodb.py
@@ -2,39 +2,12 @@ from pymongo import MongoClient
 from mongo_uri import uri
 
 MONGO_URI = uri()
+collection = lambda MONGO_URI: MongoClient(MONGO_URI)['notes']['notes']
 
-def run_mongo(MONGO_URI):
-    client = MongoClient(MONGO_URI)        
-    db = client['notes']
-    collection = db['notes']
-    return collection
-
-def set_data(data):
-    collection = run_mongo(MONGO_URI)
-    collection.insert_one(data)
-
-def get_data():
-    collection = run_mongo(MONGO_URI)
-    results = collection.find({'available': True}) 
-    return results
-
-def get_data_one(id_data):
-    collection =  run_mongo(MONGO_URI)
-    result = collection.find_one({'_id': id_data})
-    return result
-
-def remove_data(id_data):
-    collection =  run_mongo(MONGO_URI)
-    collection.update_one({'_id': id_data}, {'$set': {'available': False}})
-
-def update_data(id_data, data):
-    collection =  run_mongo(MONGO_URI)
-    collection.update_one({'_id': id_data}, {'$set': data})
-
-def delete_db():
-    collection = run_mongo(MONGO_URI)
-    collection.delete_many({})
-
-def count_db():
-    collection = run_mongo(MONGO_URI)
-    return collection.count_documents({'available': True})
+set_data = lambda data: collection(MONGO_URI).insert_one(data)
+get_data = lambda: collection(MONGO_URI).find({'available': True}) 
+get_data_one = lambda id_data: collection(MONGO_URI).find_one({'_id': id_data})
+remove_data = lambda id_data: collection(MONGO_URI).update_one({'_id': id_data}, {'$set': {'available': False}})
+update_data = lambda id_data, data: collection(MONGO_URI).update_one({'_id': id_data}, {'$set': data})
+delete_db = lambda: collection(MONGO_URI).delete_many({})
+count_db = lambda: collection(MONGO_URI).count_documents({'available': True})
